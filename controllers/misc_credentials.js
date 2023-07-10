fe54e1aa198a65c2a69280ff4445dba36e55a8db
19VISHAL98 <vishalkumar8673@gmail.com>
2023-07-10 14:49:49 +0530

    mices

--- controllers/misc_credentials.js
+++ controllers/misc_credentials.js
@@ -1,20 +1,21 @@
 const { write } = require("../conn");
 const { encrypt, decrypt } = require("../utilities/encryption-decryption");
 
-const SQL_CHECK_MISC_BY_ID = "select * from misc_credentials where misc_id = ? and is_deleted = 0  limit 1";
+const SQL_CHECK_MISC_BY_ID = "select * from misc_credentials where server_credential = ? and is_deleted = 0 ";
 const SQL_CHECK_MISC = "select * from misc_credentials where is_deleted = 0";
-const SQL_INSERT_MISC = "insert into misc_credentials(service_name, alias, login_url, username, password, notes ) values (?,?,?,?,?,?)";
+const SQL_INSERT_MISC = "insert into misc_credentials(service_name, alias, login_url, username, password, notes  , server_credential) values (?,?,?,?,?,? ,?)";
 const SQL_UPDATE_MISC = "update misc_credentials set service_name= ?, alias = ?, login_url = ?, username = ?, password= ?, notes = ?  where misc_id = ? limit 1 ";
 const SQL_DELETE_MISC = "update misc_credentials set is_deleted = 1 where misc_id = ? limit 1 ";
+const SQL_CHECK_SERVER = "select * from server_credentials where is_deleted = 0";
 
 
 
 
 async function insertMiscCredentials(req, res) {
     try {
-        let { service_name, alias, login_url, username, password, notes} = req.body;
+        let { service_name, alias, login_url, username, password, notes , server_credential} = req.body;
         password = await encrypt(password)
-        await write.query(SQL_INSERT_MISC, [service_name, alias, login_url, username, password, notes ]);
+        await write.query(SQL_INSERT_MISC, [service_name, alias, login_url, username, password, notes  , server_credential]);
         return res.status(200).send({ status: true, message: "Misc credentails inserted successfully" });
     }
     catch (err) {
@@ -56,9 +57,16 @@ async function findAllMiscCredentials(req, res) {
 
 async function miscFindById(req, res) {
     try {
-        let [misc] = await write.query(SQL_CHECK_MISC_BY_ID, [req.params.misc_id]);
-        misc[0].password = await decrypt(misc[0].password)
-        return res.status(200).send({ status: true, data: misc });
+
+        let [server] = await write.query(SQL_CHECK_SERVER);
+        for(let x of server){
+            let [misc] = await write.query(SQL_CHECK_MISC_BY_ID, [x.server_id]);
+            for(let y of misc){
+                y.password = await decrypt(y.password)
+            }
+            x.misc = misc
+        }
+        return res.status(200).send({ status: true, data: server });
     }
     catch (err) {
         return res.status(400).json({ status: false, message: "Something went wrong" })
